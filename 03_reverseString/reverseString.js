964dbe63f69a8f1eccc1e4fc5df049c49482e91a
Lukas Bauza <lukasbauza.lb@proton.me>
2024-09-27 17:00:20 +0100

    03_reverseString Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var splitString = string.split("");
+    var reverseArray = splitString.reverse();
+    return reverseArray.join("");
 };
 
 // Do not edit below this line
